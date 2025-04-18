a6806a3e6b3c3469c2e3f117e19bddbbc0579291
mathew252993 <funtowntales@gmail.com>
2025-04-18 13:42:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const button = document.getElementById('magic-button');
-const text = document.getElementById('dynamic-text');
 
-button.addEventListener('click', () => {
-  text.innerText = "✨ You clicked the magic button! ✨";
-});
